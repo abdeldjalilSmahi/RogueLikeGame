bc96770668515f047f9911922f71015689daa295
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-02-11 18:05:45 +0100

    initialisation de la classe ScoreDeCaractéristique en créant le constructeur

--- src/main/java/fr/uvsq/pglp/roguelike/personnage/ScoreDeCaracteristique.java
+++ src/main/java/fr/uvsq/pglp/roguelike/personnage/ScoreDeCaracteristique.java
@@ -1,4 +1,21 @@
 package fr.uvsq.pglp.roguelike.personnage;
 
 public class ScoreDeCaracteristique {
+
+  private final int val;
+  private final int mod;
+
+  public ScoreDeCaracteristique(int val) {
+    if (val < 1) {
+      throw new IllegalArgumentException("la valeur de la caractéristique doit etre sup à 1");
+    }
+    if (val > 21) {
+      throw new IllegalArgumentException("la valeur de la caractéristique doit etre inférieur 21");
+    }
+    this.val = val;
+    this.mod = (val - 10) / 2;
+  }
+
+
+
 }
