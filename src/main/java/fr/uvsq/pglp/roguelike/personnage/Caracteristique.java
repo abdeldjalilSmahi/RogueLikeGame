f0e85f70bcca0bf202980dc28187a744b5efb776
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-02-11 19:31:10 +0100

    revision de checkstyle de l'enumeration

--- src/main/java/fr/uvsq/pglp/roguelike/personnage/Caracteristique.java
+++ src/main/java/fr/uvsq/pglp/roguelike/personnage/Caracteristique.java
@@ -1,21 +1,31 @@
+package fr.uvsq.pglp.roguelike.personnage;
+
 /**
- *
  * Enumération des différentes caractéristiques d'un personnage.
  *
  * @author Djalil_Oussama
  */
-package fr.uvsq.pglp.roguelike.personnage;
+
+
 public enum Caracteristique {
-  /** La force du personnage */
-   FOR,
-   /* La dextérité du personnage */
-   DEX,
-   /* La constitution du personnage */
-   CON,
-   /* L'intelligence du personnage */
-   INT,
-   /* La sagesse du personnage */
-   SAG,
-   /* Le charisme du personnage */
+  /**
+   * La force du personnage.
+   */
+  FOR,
+  /**
+   * La dextérité du personnage.
+   */
+  DEX,
+  /**
+   * La constitution du personnage.
+   */
+  CON,
+  /**
+   * L'intelligence du personnage.
+   */
+  INT,
+  /* La sagesse du personnage. */
+  SAG,
+  /* Le charisme du personnage. */
   CHAR
 }
