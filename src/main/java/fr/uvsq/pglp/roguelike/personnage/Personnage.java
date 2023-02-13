df462a8194250f5672f3083cd3fa10c06a076bd1
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-02-13 21:52:35 +0100

    gestion du conflit

--- src/main/java/fr/uvsq/pglp/roguelike/personnage/Personnage.java
+++ src/main/java/fr/uvsq/pglp/roguelike/personnage/Personnage.java
@@ -3,9 +3,4 @@ package fr.uvsq.pglp.roguelike.personnage;
 import java.util.Map;
 
 public class Personnage {
-  private String name;
-  private int PV ;
-  private int defense ;
-  private int score_caracteristique_force ;
-  private Map<Caracteristique, ScoreDeCaracteristique> scores_caracteristique ;
 }
