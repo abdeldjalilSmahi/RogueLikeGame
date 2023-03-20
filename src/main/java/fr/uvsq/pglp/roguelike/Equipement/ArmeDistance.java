72b83711b040e766b95fad020a310eac10976a68
Oussama Bouakaz <bkzoussama6@gmail.com>
2023-03-20 10:52:20 +0100

    Creer une classe Armedistance qui implemente l'interface Equipement

---
+++ src/main/java/fr/uvsq/pglp/roguelike/Equipement/ArmeDistance.java
@@ -0,0 +1,19 @@
+package fr.uvsq.pglp.roguelike.Equipement;
+
+import java.util.Random;
+
+public class ArmeDistance extends ArmeContact{
+
+
+  private ArmeContactType armecontact;
+  private Random random = new Random(new Random().nextInt());
+  private int porte;
+  public ArmeDistance(ArmeContactType armecontact) {
+  }
+
+
+  private void calculdegat(){
+
+  }
+
+}
