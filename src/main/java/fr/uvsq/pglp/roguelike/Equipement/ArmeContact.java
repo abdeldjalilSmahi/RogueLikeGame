c1942b3d4ed9d4e64498e07e3cd2e47a2b09eab9
Oussama Bouakaz <bkzoussama6@gmail.com>
2023-03-20 10:35:14 +0100

    Creer une classe Armecontact qui implemente l'interface Equipement

---
+++ src/main/java/fr/uvsq/pglp/roguelike/Equipement/ArmeContact.java
@@ -0,0 +1,19 @@
+package fr.uvsq.pglp.roguelike.Equipement;
+
+import java.util.Random;
+
+public class ArmeContact implements Equipement {
+
+private ArmeContactType armecontact;
+private int degat;
+private Random random = new Random(new Random().nextInt());
+
+  public ArmeContact() {
+
+  }
+
+
+  private void calculdegat(){
+
+  }
+}
