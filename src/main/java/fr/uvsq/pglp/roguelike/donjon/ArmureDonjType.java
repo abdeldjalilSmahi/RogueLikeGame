3b04d29515879488f661e56944b1e089d32a9af7
amir <naceri.amir@gmail.com>
2023-04-23 15:17:36 +0200

    checkstyle de la classe ArmureDonjonType

--- src/main/java/fr/uvsq/pglp/roguelike/donjon/ArmureDonjType.java
+++ src/main/java/fr/uvsq/pglp/roguelike/donjon/ArmureDonjType.java
@@ -3,18 +3,17 @@ package fr.uvsq.pglp.roguelike.donjon;
 import fr.uvsq.pglp.roguelike.equipement.Armure;
 import fr.uvsq.pglp.roguelike.equipement.ArmureType;
 
-
 public enum ArmureDonjType {
-  PETIT_BOUCLIER(new Armure(ArmureType.PETIT_BOUCLIER)," o "),
-  CUIR(new Armure(ArmureType.CUIR)," c "),
-  GRAND_BOUCLIER(new Armure(ArmureType.GRAND_BOUCLIER)," O "),
-  CUIR_RENFORCE(new Armure(ArmureType.CUIR_RENFORCE)," C "),
-  DEMI_PLAQUE(new Armure(ArmureType.DEMI_PLAQUE)," ^ ");
+  PETIT_BOUCLIER(new Armure(ArmureType.PETIT_BOUCLIER), " o "),
+  CUIR(new Armure(ArmureType.CUIR), " c "),
+  GRAND_BOUCLIER(new Armure(ArmureType.GRAND_BOUCLIER), " O "),
+  CUIR_RENFORCE(new Armure(ArmureType.CUIR_RENFORCE), " C "),
+  DEMI_PLAQUE(new Armure(ArmureType.DEMI_PLAQUE), " ^ ");
 
   Armure armure;
   String asciiChar;
 
- ArmureDonjType(Armure armure, String asciiChar) {
+  ArmureDonjType(Armure armure, String asciiChar) {
     this.armure = armure;
     this.asciiChar = asciiChar;
   }
