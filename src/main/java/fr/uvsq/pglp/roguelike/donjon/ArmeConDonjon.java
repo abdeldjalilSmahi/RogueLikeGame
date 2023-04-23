a7230b564fd23851a5c2e3d77e0a0fd528791c7f
amir <naceri.amir@gmail.com>
2023-04-23 15:02:10 +0200

    checkstyle de la class ArmeConDonjon

--- src/main/java/fr/uvsq/pglp/roguelike/donjon/ArmeConDonjon.java
+++ src/main/java/fr/uvsq/pglp/roguelike/donjon/ArmeConDonjon.java
@@ -4,13 +4,14 @@ import fr.uvsq.pglp.roguelike.equipement.ArmeContact;
 import java.awt.geom.Point2D;
 import org.fusesource.jansi.Ansi.Color;
 
-public class ArmeConDonjon extends DonjonObject{
+public class ArmeConDonjon extends DonjonObject {
   private ArmeContact armeContact;
   private ArmeConDonjType armeConDonjType;
+
   public ArmeConDonjon(Point2D position, Color color, ArmeConDonjType armeConDonjType) {
     super(position, armeConDonjType.getAsciiChar(), color);
     this.armeConDonjType = armeConDonjType;
-    this.armeContact = armeConDonjType.getArmeContact() ;
+    this.armeContact = armeConDonjType.getArmeContact();
   }
 
   public ArmeContact getArmeContact() {
