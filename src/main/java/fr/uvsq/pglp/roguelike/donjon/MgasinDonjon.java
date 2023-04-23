2d0c334df7a657446a685e77493bab28943b4b9f
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-04-23 18:38:08 +0200

    gestion du magasin

--- src/main/java/fr/uvsq/pglp/roguelike/donjon/MgasinDonjon.java
+++ src/main/java/fr/uvsq/pglp/roguelike/donjon/MgasinDonjon.java
@@ -10,24 +10,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MgasinDonjon extends DonjonObject{
-    private List<ArmeContact> armeContacts;
-    private List<ArmeDistance> armeDistances;
-    private List<Armure> armures;
+    private List<ArmureDonjType> armuresDonj;
+    private List<ArmeDistDonjType> armesDist;
+    private List<ArmeConDonjType> armesCont;
     private Personnage pj;
     public MgasinDonjon(Point2D position) {
         super(position, " M ", Ansi.Color.MAGENTA);
-        this.armeContacts =
-                Arrays.asList(ArmeContactType.values()).stream()
-                        .map(e -> new ArmeContact(e))
-                        .collect(Collectors.toList());
-        this.armeDistances =
-                Arrays.asList(ArmeDistanceType.values()).stream()
-                        .map(e -> new ArmeDistance(e))
-                        .collect(Collectors.toList());
-        this.armures =
-                Arrays.asList(ArmureType.values()).stream()
-                        .map(e -> new Armure(e))
-                        .collect(Collectors.toList());
+        this.armesCont = Arrays.asList(ArmeConDonjType.values());
+        this.armesDist = Arrays.asList(ArmeDistDonjType.values());
+        this.armuresDonj = Arrays.asList(ArmureDonjType.values());
     }
     public Personnage getPj() {
         return pj;
@@ -37,18 +28,23 @@ public class MgasinDonjon extends DonjonObject{
         this.pj = pj;
     }
     public void presenter() {
-        // Afficher la liste des objets disponibles à la vente et demander à l'acheteur de choisir
+//        // Afficher la liste des objets disponibles à la vente et demander à l'acheteur de choisir
         System.out.println("Objets disponibles à la vente:");
-        for (ArmeContact objet : armeContacts) {
-            System.out.println(objet.getArmeContactType().name() + " - " + objet.getPrix() + " pièces d'or");
+        System.out.println("Les armes contacts");
+        for (ArmeConDonjType armeConDonjType : armesCont) {
+            System.out.println(armeConDonjType.getArmeContact().getArmeContactType().name() + " - "+ armeConDonjType.getAsciiChar() + " - " + armeConDonjType.getArmeContact().getPrix() + " pièces d'or");
         }
-        for (ArmeDistance objet : armeDistances) {
-            System.out.println(objet.getArmeDistanceType().name() + " - " + objet.getPrix() + " pièces d'or");
+        System.out.println("------------------------------");
+        System.out.println("Les armes distants");
+        for (ArmeDistDonjType armeDistDonjType : armesDist) {
+            System.out.println(armeDistDonjType.getArmeDistance().getArmeDistanceType().name() + " - "+ armeDistDonjType.getAsciiChar() + " - " + armeDistDonjType.getArmeDistance().getPrix() + " pièces d'or");
         }
-        for (Armure objet : armures) {
-            System.out.println(objet.getArmureType().name() + " - " + objet.getPrix() + " pièces d'or");
+        System.out.println("------------------------------");
+        System.out.println("Les armures");
+        for (ArmureDonjType armureDonjType : armuresDonj) {
+            System.out.println(armureDonjType.getArmure().getArmureType().name() + " - "+ armureDonjType.getAsciiChar() + " - " + armureDonjType.getArmure().getPrix() + " pièces d'or");
         }
-        System.out.println("Quel objet voulez-vous acheter? (entrez le numéro de l'objet ou 0 pour annuler)");
 
     }
+
 }
