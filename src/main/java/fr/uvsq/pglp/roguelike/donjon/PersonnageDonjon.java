184fd4e8eecffd38f624b4f7e17a404b452c90b9
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-04-24 00:10:40 +0200

    checkstyle du PersonnageDonjon

--- src/main/java/fr/uvsq/pglp/roguelike/donjon/PersonnageDonjon.java
+++ src/main/java/fr/uvsq/pglp/roguelike/donjon/PersonnageDonjon.java
@@ -3,17 +3,22 @@ package fr.uvsq.pglp.roguelike.donjon;
 import fr.uvsq.pglp.roguelike.personnage.Personnage;
 import java.awt.geom.Point2D;
 import java.util.Map;
-
-import javax.sound.midi.Soundbank;
-
-
-
 import org.fusesource.jansi.Ansi;
 
+/**
+ * La classe personnage dans le donjon.
+ */
 public class PersonnageDonjon extends Creature {
 
+  /** Personnage a enrichir par une position. */
   private Personnage personnage;
 
+  /**
+   * Constructeur de personnageDonjon.
+   *
+   * @param personnage le personage.
+   * @param position la position du personnage.
+   */
   public PersonnageDonjon(Personnage personnage, Point2D position) {
     super(position, " @ ", Ansi.Color.CYAN);
     this.personnage = personnage;
@@ -25,17 +30,22 @@ public class PersonnageDonjon extends Creature {
 
   @Override
   public boolean canMoveTo(Point2D newPosition, Map<Point2D, DonjonObject> map) {
-    if (map.get(newPosition).getAsciiChar()!=DonjonComponentType.SOL.getAsciiChar()) {
+    if (map.get(newPosition).getAsciiChar() != DonjonComponentType.SOL.getAsciiChar()) {
       System.out.println("yeah");
       return false;
     }
     return true;
   }
 
+  /**
+   * Méthode permet de bouger le joueur.
+   *
+   * @param newPosition la nvl position.
+   * @param donjon le donjon courant.
+   */
   public void moveTo(Point2D newPosition, Donjon donjon) {
     if (canMoveTo(newPosition, donjon.getAllElements())) {
       donjon.swapObjects(this, donjon.getObject(newPosition));
     }
-
   }
 }
