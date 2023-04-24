28755e2c4e0098d48df1ddd31ca23c70764420b9
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2023-04-24 12:44:17 +0200

    ajout des test

---
+++ src/test/java/fr/uvsq/pglp/roguelike/donjon/ArmeConDonTest.java
@@ -0,0 +1,20 @@
+package fr.uvsq.pglp.roguelike.donjon;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import fr.uvsq.pglp.roguelike.equipement.ArmeContact;
+import fr.uvsq.pglp.roguelike.equipement.ArmeContactType;
+import java.awt.geom.Point2D;
+import org.fusesource.jansi.Ansi.Color;
+import org.junit.jupiter.api.Test;
+
+public class ArmeConDonTest {
+  @Test
+  public void armeConDonjonTest(){
+    ArmeConDonjon armeConDonjon = new ArmeConDonjon(new Point2D.Double(1,1), Color.CYAN, ArmeConDonjType.BATON_FEREE);
+    ArmeContact armeContact = new ArmeContact(ArmeContactType.BATON_FEREE);
+    assertEquals()
+
+  }
+
+}
